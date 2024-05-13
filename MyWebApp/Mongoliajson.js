37b3d2331bab6d87095577e0c22dc14065a4fd80
LitsaWoo <127156125+LitsaWoo@users.noreply.github.com>
2024-05-13 16:17:40 -0600

    Update Mongoliajson.js

--- MyWebApp/Mongoliajson.js
+++ MyWebApp/Mongoliajson.js
@@ -94,7 +94,7 @@ controlLayers.addBaseLayer(Esri_WorldImagery, "Terrain Basemap");
          connect: true,
          range: {
            'min': 1997,
-           'max': 2022
+           'max': 2023
          },
          step: 1,
          connect: true,
